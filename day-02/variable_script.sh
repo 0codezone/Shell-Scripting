75aa10413ec8a99529e3f9b5159366efe1722d44
Mohit Darji <mohittailor997@gmail.com>
2025-07-13 18:15:45 +0530

    setup environtment for shell scripting and test it

---
+++ day-02/variable_script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#-------------------------waht is variable is Shell Scripting ---------------------
+
+name="mohit"
+echo "my name is, ${name}" 
+
+age=24
+echo "my age is, ${age}"
+
+
---
+++ day-1/hello.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# first to know  script konse shell me chal rhi h : shebang
+
+
+#this script is for ME
+
+echo "ME : this is first day to learn Shell Script"
+
+echo "learning from trainwithshumbham YT to become cloud or devops"
+
